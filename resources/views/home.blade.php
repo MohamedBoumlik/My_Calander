2963c1555dcbdc8b8515f35b7d417196fc290de8
MohamedBoumlik <mohamed.boumlik.19@gmail.com>
2022-05-28 17:39:54 +0100

    adding calendar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,13 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <br />
+        <h1 class="text-center text-danger"><u>My Calendar</u></h1>
+        <br />
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div id="calendar"></div>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,17 +6,17 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link href='fullcalendar/main.css' rel='stylesheet' />
-    <script src='fullcalendar/main.js'></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -85,5 +85,123 @@
             @yield('content')
         </main>
     </div>
+    <script>
+        $(document).ready(function () {
+
+            $.ajaxSetup({
+                headers:{
+                    'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            var calendar = $('#calendar').fullCalendar({
+                editable:true,
+                header:{
+                    left:'prev,next today',
+                    center:'title',
+                    right:'month,agendaWeek,agendaDay'
+                },
+                events:'/full-calender',
+                selectable:true,
+                selectHelper: true,
+                select:function(start, end, allDay)
+                {
+                    var title = prompt('Event Title:');
+
+                    if(title)
+                    {
+                        var start = $.fullCalendar.formatDate(start, 'Y-MM-DD HH:mm:ss');
+
+                        var end = $.fullCalendar.formatDate(end, 'Y-MM-DD HH:mm:ss');
+
+                        $.ajax({
+                            url:"/full-calender/action",
+                            type:"POST",
+                            data:{
+                                title: title,
+                                start: start,
+                                end: end,
+                                type: 'add'
+                            },
+                            success:function(data)
+                            {
+                                calendar.fullCalendar('refetchEvents');
+                                alert("Event Created Successfully");
+                            }
+                        })
+                    }
+                },
+                editable:true,
+                eventResize: function(event, delta)
+                {
+                    var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
+                    var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
+                    var title = event.title;
+                    var id = event.id;
+                    $.ajax({
+                        url:"/full-calender/action",
+                        type:"POST",
+                        data:{
+                            title: title,
+                            start: start,
+                            end: end,
+                            id: id,
+                            type: 'update'
+                        },
+                        success:function(response)
+                        {
+                            calendar.fullCalendar('refetchEvents');
+                            alert("Event Updated Successfully");
+                        }
+                    })
+                },
+                eventDrop: function(event, delta)
+                {
+                    var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
+                    var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
+                    var title = event.title;
+                    var id = event.id;
+                    $.ajax({
+                        url:"/full-calender/action",
+                        type:"POST",
+                        data:{
+                            title: title,
+                            start: start,
+                            end: end,
+                            id: id,
+                            type: 'update'
+                        },
+                        success:function(response)
+                        {
+                            calendar.fullCalendar('refetchEvents');
+                            alert("Event Updated Successfully");
+                        }
+                    })
+                },
+
+                eventClick:function(event)
+                {
+                    if(confirm("Are you sure you want to remove it?"))
+                    {
+                        var id = event.id;
+                        $.ajax({
+                            url:"/full-calender/action",
+                            type:"POST",
+                            data:{
+                                id:id,
+                                type:"delete"
+                            },
+                            success:function(response)
+                            {
+                                calendar.fullCalendar('refetchEvents');
+                                alert("Event Deleted Successfully");
+                            }
+                        })
+                    }
+                }
+            });
+
+        });
+    </script>
 </body>
 </html>
